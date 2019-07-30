40fe4f4f04ade65205fc3da4861fd2db8a485b9b
Leonardo Souza Faria <leonardo.souza.faria@everis.com>
2019-07-30 16:01:54 -0300

    Entrega atividade casa do codigo SpringBoot

---
+++ src/main/java/com/br/spring/PropriedadeRepository.java
@@ -0,0 +1,16 @@
+package com.br.spring;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+
+public interface PropriedadeRepository extends PagingAndSortingRepository<Propriedade, String> {
+	
+	@Query("SELECT c from Propriedade c where c.nome like %:filtro% order by categoria,subcategoria,nome")
+	public List<Propriedade> findByFiltro(@Param("filtro") String filtro);
+
+}
+	
